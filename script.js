cfc35a257e260a6d27b516bc38a4ad633c706d7e
Sulman <sulman.malik@hotmail.co.uk>
2025-09-27 10:52:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-AOS.init({
-  duration: 800,
-  once: true
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
 });
